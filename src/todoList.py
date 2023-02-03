e14f2e4ca57b9c5b4e918c0584bffe0c3cdb33e3
Ubuntu <ubuntu@ip-172-31-14-160.ec2.internal>
2023-02-03 11:04:54 +0000

    Revertimos cambios en las pruebas unitarias

--- src/todoList.py
+++ src/todoList.py
@@ -10,15 +10,15 @@ from botocore.exceptions import ClientError
 def get_table(dynamodb=None):
     if not dynamodb:
         #AGB utilizamos el comando pragma: no cover para aumentar el porcentaje de coverage
-        URL = os.environ['ENDPOINT_OVERRIDE'] # pragma: no cover
-        if URL:# pragma: no cover
-            print('URL dynamoDB:'+URL)# pragma: no cover
-            boto3.client = functools.partial(boto3.client, endpoint_url=URL)# pragma: no cover
+        URL = os.environ['ENDPOINT_OVERRIDE'] 
+        if URL:
+            print('URL dynamoDB:'+URL)
+            boto3.client = functools.partial(boto3.client, endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource,
-                                               endpoint_url=URL)# pragma: no cover
-        dynamodb = boto3.resource("dynamodb")# pragma: no cover
+                                               endpoint_url=URL)
+        dynamodb = boto3.resource("dynamodb")
     # fetch todo from the database
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])# pragma: no cover
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
 
 
